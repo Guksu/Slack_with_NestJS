12451607b6f005cda59b839a532751a9edf75a00
Guksu <yung3152@daum.net>
2022-09-14 20:10:54 +0900

    [2022-09-14]

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// ---------------NestJs판 dotenv---------------------------//
+import { ConfigModule, ConfigService } from '@nestjs/config';
+//---------------------------------------------------------//
+import { LoggerMiddleware } from './middleware/logger.middleware';
+
+//-----------------------env를 AWS나 외부에서 관리하는 경우에는 load 함수를 사용한다---------------//
+const getEnv = async () => {
+  // const keyReq = await axios.get("비밀키요청")
+  return {
+    DB_NAME: '123',
+    NAME: 'MIN',
+  };
+};
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [getEnv] })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  // ---------------process.env.SAMPLE 이런방식 대신 ConfigService를 사용하면 Nest가 env도 제어하므로 나중에 테스트하기에도 편하다---------------//
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return this.configService.get('SAMPLE');
   }
 }
